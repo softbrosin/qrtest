455ca3a59dc941e691b6c388de993e389608c444
softbrosin <147948676+softbrosin@users.noreply.github.com>
2023-10-15 19:21:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-function domReady(fn) { 
-    if ( 
-        document.readyState === "complete" || 
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
         document.readyState === "interactive"
-    ) { 
-        setTimeout(fn, 1000); 
-    } else { 
-        document.addEventListener("DOMContentLoaded", fn); 
-    } 
-} 
-  
-domReady(function () { 
-  
-    // If found you qr code 
-    function onScanSuccess(decodeText, decodeResult) { 
-        alert("You Qr is : " + decodeText, decodeResult); 
-    } 
-  
-    let htmlscanner = new Html5QrcodeScanner( 
-        "my-qr-reader", 
-        { fps: 10, qrbos: 250 } 
-    ); 
-    htmlscanner.render(onScanSuccess); 
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        document.querySelector('#pigmy-act-no input').value =  decodeText, decodeResult;
+        document.getElementById('html5-qrcode-button-camera-stop').click();
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
 });
+
+document.querySelector('#my-qr-reader__scan_region img').src = "asset/tenor.gif"
